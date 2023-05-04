ad0d592415ebf6b152cc85883d7d9f2f6dcbd19e
SoryungLEE <84223423+SoryungLEE@users.noreply.github.com>
2023-05-04 15:54:29 +0900

    Update 0409_Add_Params.py

--- 0409_Add_Params.py
+++ 0409_Add_Params.py
@@ -18,7 +18,7 @@ col1, col2 = st.columns([1, 8])
 col1.image(im, width=100)
 col2.write("# 지방상수도 함내 계량기 동파 예측")
 col2.write("### (Predicting meter freezes in municipal water boxes)")
-
+col2.write("")
 
 st.write("##### 📌 예측을 위한 데이터를 입력하세요(Select Data)")
 with st.form('My Form'):
